e6d8745a16fa964ed421367eba9b5a47867ba7ec
Steve <hiephuynh200499@gmail.com>
2024-08-04 19:18:08 +0700

    #17 - implement unit-testing for CURD realm with todo service

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,14 +1,20 @@
+import Realm from 'realm';
+
 // store
-import {realm} from 'src/store/realm';
+import {realm as realmStore} from 'src/store/realm';
 import {TodoSchemeName} from 'src/store/todo/todo.scheme';
 
 // type
 import type {Todo} from 'src/store/todo/type';
 
-class TodoService {
+export class TodoService {
+  private realm: Realm;
+  constructor(realm: Realm) {
+    this.realm = realm;
+  }
   public getTodo(): Array<Todo> {
     try {
-      const data = realm.objects(TodoSchemeName);
+      const data = this.realm.objects(TodoSchemeName);
       return JSON.parse(JSON.stringify(data));
     } catch (e) {
       return [];
@@ -16,15 +22,15 @@ class TodoService {
   }
 
   public addTodo(todo: Todo) {
-    realm.write(() => {
-      realm.create(TodoSchemeName, todo);
+    this.realm.write(() => {
+      this.realm.create(TodoSchemeName, todo);
     });
   }
 
   public updateCompleteTodo(id: string) {
-    const item = realm.objectForPrimaryKey(TodoSchemeName, id);
+    const item = this.realm.objectForPrimaryKey(TodoSchemeName, id);
     if (item) {
-      realm.write(() => {
+      this.realm.write(() => {
         item.isComplete = !item.isComplete;
         item.updatedAt = new Date();
       });
@@ -32,13 +38,13 @@ class TodoService {
   }
 
   public deleteTodo(id: string) {
-    realm.write(() => {
-      const deleteItem = realm.objectForPrimaryKey(TodoSchemeName, id);
+    this.realm.write(() => {
+      const deleteItem = this.realm.objectForPrimaryKey(TodoSchemeName, id);
       if (deleteItem) {
-        realm.delete(deleteItem);
+        this.realm.delete(deleteItem);
       }
     });
   }
 }
 
-export const todoService = Object.freeze(new TodoService());
+export const todoService = Object.freeze(new TodoService(realmStore));
