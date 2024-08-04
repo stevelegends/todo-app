a9804d0113362d8e25bfec9ef6cdac4aefe4672c
Steve <hiephuynh200499@gmail.com>
2024-08-04 13:40:45 +0700

    #13 - implement CI/CD build android with github action

---
+++ scripts/clean-build-resources.sh
@@ -0,0 +1,21 @@
+rm -rf android/app/src/main/res/raw/node_modules*
+rm -rf android/app/src/main/res/raw/app*
+rm -rf android/app/src/main/res/raw/assets* 
+
+rm -rf android/app/src/main/res/drawable-xxxhdpi/node_modules*
+rm -rf android/app/src/main/res/drawable-xxhdpi/node_modules*
+rm -rf android/app/src/main/res/drawable-xhdpi/node_modules*
+rm -rf android/app/src/main/res/drawable-hdpi/node_modules*
+rm -rf android/app/src/main/res/drawable-mdpi/node_modules*
+
+rm -rf android/app/src/main/res/drawable-xxxhdpi/app*
+rm -rf android/app/src/main/res/drawable-xxhdpi/app*
+rm -rf android/app/src/main/res/drawable-xhdpi/app*
+rm -rf android/app/src/main/res/drawable-hdpi/app*
+rm -rf android/app/src/main/res/drawable-mdpi/app*
+
+rm -rf android/app/src/main/res/drawable-xxxhdpi/assets*
+rm -rf android/app/src/main/res/drawable-xxhdpi/assets*
+rm -rf android/app/src/main/res/drawable-xhdpi/assets*
+rm -rf android/app/src/main/res/drawable-hdpi/assets*
+rm -rf android/app/src/main/res/drawable-mdpi/assets*
